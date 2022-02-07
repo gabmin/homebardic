c38550544f60b070aa56d66d1edc16d8668f9493
gabmin <rlarkqals@gmail.com>
2022-02-07 18:23:11 +0900

    [edit] sever 오류 수정

--- src/shared/axios.ts
+++ src/shared/axios.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // Axios 인스턴스 설정
 const api = axios.create({
-  baseURL: "http://3.35.4.6:3000/",
-  // baseURL: "http://localhost:3000", 개발용
+  baseURL: "http://3.35.4.6:3001/",
+  // baseURL: "http://localhost:3000",
 });
 
 axios.defaults.withCredentials = true;
